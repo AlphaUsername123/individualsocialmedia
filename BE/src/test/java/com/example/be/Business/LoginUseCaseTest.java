0bb8516e90a82cfcce224009ae920644e411862d
Dan Zavalidrov <d.zavalidrov@student.fontys.nl>
2024-12-02 23:07:07 +0100

    updated tests

--- BE/src/test/java/com/example/be/Business/LoginUseCaseTest.java
+++ BE/src/test/java/com/example/be/Business/LoginUseCaseTest.java
@@ -27,9 +27,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
-@ExtendWith(SpringExtension.class)
-@SpringBootTest
-@AutoConfigureMockMvc
 class LoginUseCaseTest {
 
     @Mock
